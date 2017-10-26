f3d191e1aa42fcef6bf1932eb26b36c09143f6b6
kaiwen_Hu <hukaiwen501@163.com>
2017-10-26 13:43:44 +0800

    添加自控制器

--- UCar/UCTabBarController.m
+++ UCar/UCTabBarController.m
@@ -7,6 +7,10 @@
 //
 
 #import "UCTabBarController.h"
+#import "UCFirstPageViewController.h"
+#import "UCMapViewController.h"
+#import "UCMineViewController.h"
+#import "UCBatteryViewController.h"
 
 @interface UCTabBarController ()
 
@@ -17,7 +21,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+   
+    //添加子控制器
+    [self addChildVcWithViewController:[[UCFirstPageViewController alloc] init] backgroundColor:[UIColor redColor] title:@"shouye" unSelectImage:[UIImage imageNamed:@"homeTabarNoSelect"] andSelectImage:[UIImage imageNamed:@"homeTabarSelect"]];
     
+    [self addChildVcWithViewController:[[UCMapViewController alloc] init] backgroundColor:[UIColor blueColor] title:@"map" unSelectImage:[UIImage imageNamed:@"mapTabarNoSelect"] andSelectImage:[UIImage imageNamed:@"mapTabarSelect"]];
+    
+    [self addChildVcWithViewController:[[UCMineViewController alloc] init] backgroundColor:[UIColor blueColor] title:@"mine" unSelectImage:[UIImage imageNamed:@"homeTabarNoSelect"] andSelectImage:[UIImage imageNamed:@"homeTabarSelect"]];
+    
+    [self addChildVcWithViewController:[[UCBatteryViewController alloc] init] backgroundColor:[UIColor blueColor] title:@"Battery" unSelectImage:[UIImage imageNamed:@"homeTabarNoSelect"] andSelectImage:[UIImage imageNamed:@"homeTabarSelect"]];
+}
+
+#pragma mark -添加子控制器
+- (void)addChildVcWithViewController:(UIViewController *)viewController backgroundColor:(UIColor *)color title:(NSString *)title unSelectImage:(UIImage *)unSelectImage andSelectImage:(UIImage *)selectImage
+{
+    viewController.view.backgroundColor = color;
+    viewController.tabBarItem.title = title;
+    viewController.tabBarItem.image = [[UIImage imageNamed:@"homeTabarNoSelect"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    viewController.tabBarItem.selectedImage = [[UIImage imageNamed:@"homeTabarSelect"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    [self addChildViewController:viewController];
 }
 
 - (void)didReceiveMemoryWarning {
